96086b86977c2aaa098c7bee21d19197fd69da22
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-29 16:03:53 +0100

    Create Item class

---
+++ src/item.py
@@ -0,0 +1,11 @@
+# Write a class to hold item information.
+# The item should have name and description attributes.
+
+
+class Item:
+    def __init__(self, name, description):
+        self.name = name;
+        self.description = description
+
+    def __str__(self):
+        return f"{self.name}, {self.description}"
