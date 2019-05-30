af01545ec63fe147a53ce879955d841ab7346963
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-05-30 12:13:53 +0100

    Create Item class and add items list to Room and inventory list to Player

--- src/item.py
+++ src/item.py
@@ -4,8 +4,14 @@
 
 class Item:
     def __init__(self, name, description):
-        self.name = name;
+        self.name = name
         self.description = description
 
     def __str__(self):
         return f"{self.name}, {self.description}"
+
+    def on_take(self):
+        print(f"{self.name.upper()} has been added to your inventory.")
+
+    def on_drop(self):
+        print(f"{self.name} was dropped from your inventory.")
